a984ebbe23ce22edcfff205a8d4bbce9e206e175
Aareon Sullivan <askully13@gmail.com>
2019-04-01 14:42:56 -0700

    Add support for storing files explicitely
    This patch adds support for specifying where files such as ledger databases, config files, etc. should be stored. It also attempts to make usage of `pathlib` more consistent.

--- minibis/config.py
+++ minibis/config.py
@@ -1,16 +1,35 @@
+import sys
+import pathlib  # for type annotations
+from typing import Union
 from configparser import ConfigParser
 from pathlib import Path
 
-SRC_PATH = Path(__file__).parent.resolve()
+SRC_PATH = Path(__file__).parent
 
 
 class Config:
-    def __init__(self, conf_file="../config.ini"):
+    __slots__ = ("config", "port", "app_dir", "ledger_path",)
+
+    def __init__(self, config_path: Union[pathlib.Path, str] = None):
         self.config = ConfigParser()
-        self.config.read(SRC_PATH.joinpath(conf_file).resolve())
+
+        config_path = config_path or SRC_PATH.joinpath("../config.ini")
+        if isinstance(config_path, pathlib.Path):
+            config_path = config_path.resolve()
+
+        self.config.read(config_path)
 
         self.config = self.config["DEFAULT"]
         self.port = self.config.get("port", "5658")
-        self.ledger_path = self.config.get(
-            "ledger_path", SRC_PATH.joinpath("ledger/").resolve()
+        self.app_dir = Path(self.config.get("app-dir"))
+
+        # set default app directory for both Windows and Linux
+        if sys.platform == "win32" and self.app_dir is None:
+            self.app_dir = Path("%appdata%/Bismuth")
+        elif sys.platform != "win32" and self.app_dir is None:
+            self.app_dir = Path("$HOME/.bismuth")
+
+        # where ledger files (ledger, hyper, etc.) are stored
+        self.ledger_path = Path(
+            self.config.get("ledger_path", self.app_dir.joinpath("ledger/"))
         )
