e04632f3b14276e7ca85cd721d1e5f4b24889cb2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 03:27:05 +0000

    Adding tarfile member sanitization to extractall()

--- minibis/node.py
+++ minibis/node.py
@@ -74,7 +74,26 @@ def bootstrap(ledger_path):
         download_file("https://bismuth.cz/ledger.tar.gz", archive_path)
 
         with tarfile.open(archive_path) as tar:
-            tar.extractall(SRC_PATH.joinpath("ledger/").resolve())
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, SRC_PATH.joinpath("ledger/").resolve())
 
         logger.info(f"Bootstrap extracted to {path}")
 
